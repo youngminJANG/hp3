cded0fa9878bfd988f38fc03b8487bd60370f2ea
youngminJANG <66574097+youngminJANG@users.noreply.github.com>
2021-06-17 12:10:45 +0900

    Create menu.js

---
+++ menu.js
@@ -0,0 +1,36 @@
+var now = -1;
+var last =2;
+
+jQuery(document).ready(function() {
+  $(".nav>li").mouseover(function(){
+    $(this).children().slideDown();
+  });
+  
+  $(".nav>li").mouseleave(function(){
+    $(this).children().slideUp();
+  });
+  
+  start();
+  
+  $(".banner img").click(function(){
+    $("#modal").addClass("active");
+  });
+  $("#modal button").click(function(){
+    $("#modal").removeClass("active");
+  });
+  
+  
+  
+  
+});
+
+function start(){
+  setInterval(function(){ InOunt();},3000);
+}
+
+
+
+
+
+
+
